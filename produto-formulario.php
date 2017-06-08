07d14fd176672f9c356632d1f5ed95811e73480b
Uires Deivide <uiresdeivide1@gmail.com>
2017-06-08 15:58:25 -0300

    Formulario para cadastro de produtos

---
+++ produto-formulario.php
@@ -0,0 +1,9 @@
+<html>
+	<meta charset="UTF-8">
+	<form>
+		Nome: <input type="text" name="nome"/><br>
+		Preço: <input type="number" name="preco"/><br>
+
+		<input type="submit" name="Cadastrar"/>
+	</form>
+</html>
