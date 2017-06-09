5b62b655283c45f73fa629db2eb4380da13ea27b
Uires Deivide <uiresdeivide1@gmail.com>
2017-06-08 22:32:55 -0300

    Chamandop para o banco de dados

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,12 +1,20 @@
-<?php include("cabecalho.php"); ?>
-	
-	<?php
-	$nome = $_GET["nome"];
-	$preco = $_GET["preco"];
-	?>
-	<p class="alert-success">
-	Produto <?= $nome; ?> foi adicionado com sucesso!<br>
-	O preço do produto é : R$ <?= $preco; ?>
-	</p>
+<?php
+include("cabecalho.php");
 
-<?php include("rodape.php"); ?>
+$nome = $_GET["nome"];
+$preco = $_GET["preco"];
+
+$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+
+if(mysqli_query($conexao, $query)) {
+?>
+<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php
+} else {
+?>
+<p class="alert-danger">O produto <? = $nome; ?> não foi adicionado</p>
+<?php
+}
+
+?>
