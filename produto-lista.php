1b6f8309f28c4b70c9afed3a3574e789eb96e74d
Uires Deivide <uiresdeivide1@gmail.com>
2017-06-26 20:56:38 -0300

    Trabalhando com funções

--- produto-lista.php
+++ produto-lista.php
@@ -4,31 +4,27 @@
 
 <?php include("cabecalho.php"); ?>
 <?php include("conecta.php"); ?>
-<?php
-function buscarProdutos($conexaoComOBancoDeDados, $query, $produtosBuscados){
-	$query =  mysqli_query($conexaoComOBancoDeDados, "select * from produtos");
-	while($produtosBuscados = mysqli_fetch_assoc($query){
-		
 
+<?php
+
+function buscarProdutos($conexaoComOBancoDeDados){
+	$produtos = array();
+	$resultado =  mysqli_query($conexaoComOBancoDeDados, "select * from produtos");
+
+	while($produto = mysqli_fetch_assoc($resultado)){
+		array_push($produtos, $produto);
 	}
-	return echo $produtosBuscados['nome'] . " R$" . $produtosBuscados['preco'] . "</br>";
+
+	return $produtos;
 
 }
 
 
-
-?>
-
-<?php
-
-$resultadoDaBucasPHP = mysqli_query($conexaoComOBancoDeDados, "select * from produtos");
-
-while($produtosBuscados = mysqli_fetch_assoc($resultadoDaBucasPHP)){
-	echo $produtosBuscados['nome'] . "o preço é: R$ " . $produtosBuscados['preco'] . "</br>" ;
+$produtos = buscarProdutos($conexaoComOBancoDeDados);
+foreach($produtos as $produto) {
+    echo $produto['nome'] . "<br/>";
 }
 
 ?>
-
-
 
 <?php include("rodape.php"); ?>
